dd4e57d500192d01d12c1e7bc6e2bdabff550c99
Yana <yanaegorova69.3@gmail.com>
2021-08-30 01:04:09 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,173 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Color Paddles',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                title: 'Color changing toys for kids',
+                text: 'Translucent paddles that children can look through to change their surroundings to different colors.',
+            },
+            {
+                title: 'Preschool science for kids',
+                text: 'Demonstrate the principles of color mixing.',
+            },
+            {
+                title: '3 different textures',
+                text: 'Each paddle is segmented with 3 different textures.',
+            },
+            {
+                title: '18 paddles included',
+                text: 'Includes 18 paddles - 3 sets of 6 colors, and activity guide.',
+            },
+            {
+                title: 'Perfect for preschool & beyond',
+                text: 'Ideal for ages 3 and up.',
+            },
+        ],
+        images: ['./img/img1.png', './img/img1-1.png', './img/img1-2.png', './img/img1-3.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Leather Hole Punch',
+        price: 29.99,
+        discount: 24.65,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Rotary hole punch and sizing tool for working with leather materials and more.',
+            },
+            {
+                text: 'Creates punch hole sizes including 2mm, 2.5mm, 3mm, 3.5mm, 4mm, and 4.5mm.',
+            },
+            {
+                text: 'Ideal for leather craft projects, creating belt holes, jewelry, applying buckles and studs; works with felt, fabric, cardboard, and more.',
+            },
+            {
+                text: 'Drop forged, heat treated steel component construction with a chrome finish.',
+            },
+            {
+                text: 'Contoured ergonomic handle offers a comfortable, textured grip and easy usability.',
+            },
+            {
+                text: 'Auto-release and safety lock functions.',
+            },
+        ],
+        images: ['./img/img2.png', './img/img2-1.png', './img/img2-2.png' , './img/img2-3.jpeg'],
+    },
+    {
+        id: 'product-3',
+        name: 'Soldering Station',
+        price: 64.90,
+        discount: 69.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'High performance analog soldering station produces up to 900° F to handle many soldering projects.',
+            },
+            {
+                text: 'Variable power control dial adjusts power from 5 watts to 40 watts for accuracy.',
+            },
+            {
+                text: 'Quality, lightweight pencil iron with cushioned foam grip provides extended comfort during long term soldering projects.',
+            },
+            {
+                text: 'Includes: a certified ST3 iron plated tip for long life and consistent performance, a built in soldering holder to safely rest your pencil, and a cleaning sponge to remove unwanted residue from soldering for next time use.',
+            },
+            {
+                text: 'UL Listed: Tested and meets independent safety standards.',
+            },
+        ],
+        images: ['./img/img3.png', './img/img3-1.png', './img/img3-2.png', './img/img3-3.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Art Storage Cart',
+        price: 98.90,
+        discount: 55.97,
+        image: './img/img4.png',
+        description: [
+            {
+                title: 'Solid beech wood studio cart',
+                text: 'The wood trolley art cart in size 35" x 28-1/3" x 15-3/4", made with solid beechwood, natural,light-weight and sturdy, can hold weight up to 88lbs. Oil finished surface super smooth and glitch-free. Hand-sanded corners and edges for maximum safety.',
+            },
+            {
+                title: '3 tier open shelves',
+                text: 'Art supplies cart has 3 open shelves for you to store whatever you want more freedom, canvas, painting brushes, paints, painting knives or other art supplies, even your daily necessities, all can near from you. There is enough room! They all can be in the cart in a rather organized fashion.',
+            },
+            {
+                title: '360° swivel noice-free wheels',
+                text: 'The storage cart with wheels offers both mobility and stability. You can use it wherever you want. From your bedroom to the living room, or your big yard, even the studio, art classroom. It\'ll be there wherever it\'s needed most. Two of the caster wheels have locking brakes for safety and security to prevent slidin.',
+            },
+            {
+                title: 'Super friendly design',
+                text: 'The painting art cart is pretty easy to assemble, 100% stress-free. The top shelf has ridges for placing your brushes, colored pencils, and any other supplies that could roll away and 1 paint cup holder hole, which can perfect for your needs, this cleverly detail design is very popular with beginners, hobbyists, and professional artists.',
+            },
+        ],
+        images: ['./img/img4.png', './img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png', './img/img4-5.jpeg', './img/img4-6.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Lighted Palm Tree',
+        price: 119.98,
+        discount: 79.93,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'The Palm Tree decorated with 96 LED light, more for trunk. Material Type: Plastic.',
+            },
+            {
+                text: 'It come with 7 foot, plug-in use adapter. Long last working hours without brightness down. Power Source Type: Electric Cord.',
+            },
+            {
+                text: 'Easy to install. Stable with 4 ground stakes for the iron stand.',
+            },
+            {
+                text: 'Unique design makes attractive appearance, perfect for decorating your home, garden and even more application.',
+            },
+            {
+                text: 'Use for across seasons, always bring the Hawaii happiness.',
+            },
+        ],
+        images: ['./img/img5.png', './img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Wireless Earbuds',
+        price: 148.00,
+        discount: 95.50,
+        image: './img/img6.png',
+        description: [
+            {
+                title: 'Your Ears Never Had It So Good',
+                text: 'Ear buds take your passion for music to new heights with booming sound that makes you feel like you\'re on stage with your favorite band.',
+            },
+            {
+                title: 'Tune Out Noises, Tune In Voices',
+                text: 'Active Noise Cancellation blocks out unwanted sounds, helping you keep your focus wherever you are; Low latency Ambient Sound mode picks up the sounds you want to hear, so you always have the perfect audio level for every moment.',
+            },
+            {
+                title: 'Feels Good While Looking Good',
+                text: 'Ear Buds have a comfortable, low profile design that\'s 10% smaller and lighter. You may get to the end of your playlist before you remember you\'re wearing them at all.',
+            },
+            {
+                title: 'Hear Clearly, Speak Confidently',
+                text: 'The earbuds’ 3 microphones and noise reduction technology ensures voices are transmitted crisply and clearly, whether you\'re chatting with a friend or leading a high stakes conference call.',
+            },
+            {
+                title: 'Power Worthy of Your Longest Playlist',
+                text: 'Indulge your love of music with a long-lasting battery that\'s up to the job — so your music doesn’t stop until you do.',
+            },
+            {
+                title: 'Tap for Total Control',
+                text: 'Control your playlist or answer calls with the tap of a finger with touch control — no more fumbling for your phone to change a song.',
+            },
+            {
+                title: 'Switch It Up',
+                text: 'With Auto Switch, the earphones detect what connection you need and instantly shifts audio to device.',
+            },
+        ],
+        images: ['./img/img6.png', './img/img6-1.png', './img/img6-2.png', './img/img6-3.png', './img/img6-4.png', './img/img6-5.png', './img/img6-6.png', './img/img6-7.png', './img/img6-8.png'],
+    },
+]
