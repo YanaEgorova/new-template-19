65ae07b25185a7d5f1d0d22bef935b89fe1a235a
Yana <yanaegorova69.3@gmail.com>
2021-08-31 15:49:22 +0300

    +

--- js/data/products.js
+++ js/data/products.js
@@ -2,7 +2,7 @@ export const products = [
     {
         id: 'product-1',
         name: 'Color Paddles',
-        price: 6.96,
+        price: 19.99,
         image: './img/img1.png',
         description: [
             {
@@ -31,8 +31,8 @@ export const products = [
     {
         id: 'product-2',
         name: 'Leather Hole Punch',
-        price: 29.99,
-        discount: 24.65,
+        price: 24.99,
+        discount: 14.99,
         image: './img/img2.png',
         description: [
             {
@@ -58,9 +58,167 @@ export const products = [
     },
     {
         id: 'product-3',
-        name: 'Soldering Station',
-        price: 64.90,
+        name: 'Plastic gasoline fuel can',
+        price: 35.32,
+        discount: 24.65,
+        image: './img/img7.png',
+        description: [
+            {
+                text: '5 Gallon Plastic Gasoline Fuel Can, not compatible with greater than 10% ethanol fuel, Holds 5 gallons (20 liters) of gas and oil mixes, Exceeds California Air Resource Board (CARB) and EPA requirements for portable fuel containers, Made of durable HDPE with barrier materials to virtually eliminate hydrocarbon emissions, Built with a wider base and lower center of gravity to reduce the chance of a can tipping over.',
+            },
+        ],
+        images: ['./img/img7.png', './img/img7-1.png', './img/img7-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Tour Bag',
+        price: 39.99,
+        discount: 39.97,
+        image: './img/img8.png',
+        description: [
+            {
+                text: 'Roomy compartment withzippered RFID blocking pocket,padded pocket to hold tablet, and tethered key clipwith LED light.',
+            },
+            {
+                text: 'Two easy access, locking front pockets.',
+            },
+            {
+                text: 'Two mesh expansion pockets hold a water bottle, umbrella or sunglasses. Rear zippered pocket. hold a water bottle, umbrella or sunglasses.',
+            },
+            {
+                text: 'Material: Cotton Polyester Blend. Size: 9" x 11.5" x 3". Strap Drop Length: 14" – 25"',
+            },
+        ],
+        images: ['./img/img8.png', './img/img8-1.png', './img/img8-2.png', './img/img8-3.png', './img/img8-4.png', './img/img8-5.png', './img/img8-6.png', './img/img8-7.png', './img/img8-8.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Shaving Strop',
+        price: 43.98,
+        discount: 55.97,
+        image: './img/img9.png',
+        description: [
+            {
+                title: 'A sharp & good razor strop',
+                text: 'The aptitude of wet shaving starts with an impressive razor and your new-fangled razor sharpener. This leather sharpening strop will relieve you retain your blade sharp and robust for years to return. It also works as a kitchen knife strop. The hard brass is sewed up perfectly for long-lasting connectivity, best in school for a little leather strop. With extreme strength and effortlessness of use, this quality cowhide strop will last for thousands of shaves.',
+            },
+            {
+                title: 'Perfect gift',
+                text: 'Leather strops are the perfect gift for the modish man. This is a gift of looking decent, sensation good, and comfort make males feel strongly handsome with their sharp straight razor made possible by this stylish strop.',
+            },
+            {
+                title: 'Strop & shave',
+                text: 'Not ever again bear the painful anguish of a dull razor. Fair strop it with a leather strop and watch your razor\'s edge rise back to life.',
+            },
+            {
+                title: 'Your lifetime & money-back guarantee',
+                text: 'We are sure guaranteed you will love straight razor strops. Our guarantee promise is humble and honest. If you don’t like the product, we will refund or exchange it for one of our products. Our shaving soaps should keep thousands of gentlemen happy concerning astonishing handsome faces created for touching and made for touching and looking great.',
+            },
+            {
+                text: 'Extra wide & 2.5 mm thick.',
+            },
+        ],
+        images: ['./img/img9.png', './img/img9-1.png', './img/img9-2.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Universal Stylus',
+        price: 49.95,
         discount: 69.99,
+        image: './img/img10.png',
+        description: [
+            {
+                title: 'Dual Tip Stylus',
+                text: 'Navigate and scroll with the universal capacitive backend tip and write or draw with the active tip.',
+            },
+            {
+                title: 'Tilt Recognition',
+                text: 'Stylus has tilt recognition so you can vary the width of your stroke.',
+            },
+            {
+                title: 'Palm Rejection Technology',
+                text: 'Only the stylus will register on your tablet, even if your hand touches your screen.',
+            },
+            {
+                title: 'Increase your productivity',
+                text: 'Scroll and navigate pages with ease, or make notes, sketch, mark up documents, and more.',
+            },
+            {
+                text: 'The Stylus lasts up to nine hours before it needs to be recharged.',
+            },
+        ],
+        images: ['./img/img10.png', './img/img10-1.png', './img/img10-2.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Moisture Meter',
+        price: 69.99,
+        discount: 69.94,
+        image: './img/img11.png',
+        description: [
+            {
+                title: 'Measures',
+                text: 'Separate settings for drywall, masonry, softwood and hardwood.',
+            },
+            {
+                title: 'No drywall damage',
+                text: 'Non-marring sensing technology protects drywall.',
+            },
+            {
+                title: 'Easy to read',
+                text: 'LEDs clearly indicate moisture level.',
+            },
+            {
+                title: 'HI visibility screen',
+                text: 'Large 2 inch (50mm) backlit white on black screen providing easier readability from any angle.',
+            },
+            {
+                title: 'Find mold',
+                text: 'Stops mold before it starts with early detection of moisture; essential water damage tool.',
+            },
+            {
+                title: 'Ergonomic',
+                text: 'Improved ergonomic handle design conforms to your hand for a more comfortable user experience.',
+            },
+        ],
+        images: ['./img/img11.png', './img/img11-1.png', './img/img11-2.png', './img/img11-3.png', './img/img11-4.png'],
+    },
+    {
+        id: 'product-8',
+        name: 'Dry Food Dispenser',
+        price: 79.99,
+        discount: 79.93,
+        image: './img/img12.png',
+        description: [
+            {
+                text: 'Heavy-duty for commercial use but also suitable for the everyday kitchen.',
+            },
+            {
+                text: 'Designed to preserve freshness up to 45 days.',
+            },
+            {
+                text: 'Each canister holds up to 17.5 ounces of cereal or dried goods.',
+            },
+            {
+                text: 'Dimensions: 6.5"L x 7"W x 15"H',
+            },
+            {
+                text: 'Dry-food dispenser with 17-1/2-ounce scratch-resistant canister.',
+            },
+            {
+                text: 'Can hold a full box of cereal; preserves freshness for up to 45 days.',
+            },
+            {
+                text: 'Easy-turn dispensing knob; portion-control system; won\'t crush cereal.',
+            },
+        ],
+        images: ['./img/img12.png', './img/img12-1.png', './img/img12-2.png', './img/img12-3.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Soldering Station',
+        price: 84.41,
+        discount: 95.55,
         image: './img/img3.png',
         description: [
             {
@@ -82,10 +240,10 @@ export const products = [
         images: ['./img/img3.png', './img/img3-1.png', './img/img3-2.png', './img/img3-3.png'],
     },
     {
-        id: 'product-4',
+        id: 'product-10',
         name: 'Art Storage Cart',
-        price: 98.90,
-        discount: 55.97,
+        price: 89.99,
+        discount: 109.91,
         image: './img/img4.png',
         description: [
             {
@@ -108,10 +266,10 @@ export const products = [
         images: ['./img/img4.png', './img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png', './img/img4-5.jpeg', './img/img4-6.png'],
     },
     {
-        id: 'product-5',
+        id: 'product-11',
         name: 'Lighted Palm Tree',
         price: 119.98,
-        discount: 79.93,
+        discount: 110.98,
         image: './img/img5.png',
         description: [
             {
@@ -133,10 +291,10 @@ export const products = [
         images: ['./img/img5.png', './img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
     },
     {
-        id: 'product-6',
+        id: 'product-12',
         name: 'Wireless Earbuds',
         price: 148.00,
-        discount: 95.50,
+        discount: 120.88,
         image: './img/img6.png',
         description: [
             {
